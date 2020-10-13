b06d473a10d0b6f25af73123b1e0015881f6bef1
hi101000 <piklenews@gmail.com>
2020-10-13 12:09:57 -0400

    adding a thing to run a file through cli args

--- shell.py
+++ shell.py
@@ -1,14 +1,18 @@
 import basic
+import sys
+try:
+	filetorun=sys.argv[1]
+	basic.run("RUN", filetorun)
+except IndexError:
+	while True:
+		text = input('basic > ')
+		if text.strip() == "": continue
+		result, error = basic.run('<stdin>', text)
 
-while True:
-	text = input('basic > ')
-	if text.strip() == "": continue
-	result, error = basic.run('<stdin>', text)
-
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+		if error:
+			print(error.as_string())
+		elif result:
+			if len(result.elements) == 1:
+				print(repr(result.elements[0]))
+			else:
+				print(repr(result))
