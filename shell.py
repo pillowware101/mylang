01f76b80cf567d0cfcf02d4e05f477be877747be
hi101000 <piklenews@gmail.com>
2020-10-13 13:21:56 -0400

    finished adding running files specified in cli args so like how 'python' launches a console but 'python main.py' runs main.py

--- shell.py
+++ shell.py
@@ -1,18 +1,21 @@
 import basic
 import sys
-try:
-	filetorun=sys.argv[1]
-	basic.run("RUN", filetorun)
-except IndexError:
-	while True:
-		text = input('basic > ')
-		if text.strip() == "": continue
-		result, error = basic.run('<stdin>', text)
 
-		if error:
-			print(error.as_string())
-		elif result:
-			if len(result.elements) == 1:
-				print(repr(result.elements[0]))
-			else:
-				print(repr(result))
+try:
+	basic.run('<stdin>', 'RUN("%s")'%sys.argv[1])
+except IndexError:
+	print('\n no such file... \n')
+	print('moving on to interactive shell\n')
+
+while True:
+	text = input('basic > ')
+	if text.strip() == "": continue
+	result, error = basic.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
