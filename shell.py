b9220880a36402593f04ba839314087cb7ebfc05
Ishan Roychowdhury <piklenews@gmail.com>
2020-10-27 22:53:20 -0400

    added ability to run file from command line without using interactive interpreter

--- shell.py
+++ shell.py
@@ -1,4 +1,12 @@
 import basic
+import sys
+
+if len(sys.argv)>1:
+	fn=sys.argv[1]
+	result, error=basic.run('<stdin>', f'RUN("{fn}")')
+	if error: print(error.as_string())
+	elif result: print(result)
+	sys.exit()
 
 while True:
 	text=input("basic> ")
